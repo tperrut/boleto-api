c9281379c7637c4b0ceaa7e52afd09f17ac07422
tperrut <41409309+tperrut@users.noreply.github.com>
2018-10-24 22:02:24 -0300

    Refactor no endponit de Pagamento de boleto. Em Atendimento e conformidade com os requisitos. Criação do endponi Cancelar boleteo

--- src/main/java/com/boleto/api/web/controller/BoletoController.java
+++ src/main/java/com/boleto/api/web/controller/BoletoController.java
@@ -7,8 +7,11 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +50,43 @@ public class BoletoController {
 		return new ResponseEntity(salvo, HttpStatus.CREATED) ;
 	} 
 	
+	/**
+	 *   --Pagar um boleto---
+	 *  Esse método da API deve alterar o status do boleto para PAID.
+	 * 
+	 * @param paynent_date 
+	 * {@code Request:	{ "payment_date" : "2018-06-30" }	 }		
+	 * 
+	 * @param id
+	 * @return 204 No Content.
+	 */
+	@PutMapping("/boleto/{id}/pagamento")
+	public ResponseEntity<Object> pagarboleto(@RequestBody Boleto boleto, @PathVariable String id) {
+
+		Optional<Boleto> boletoOptional = service.buscarPorId(id);
+
+		if (!boletoOptional.isPresent())
+			return ResponseEntity.notFound().build();
+		
+		boletoOptional.get().setDataPagamento(boleto.getDataPagamento());
+		boletoOptional.get().setStatus(EnumStatus.PAID);
+		service.salvar(boletoOptional.get());
+
+		return ResponseEntity.noContent().build();
+	}
 	
+	@DeleteMapping("/boleto/{id}")
+	public ResponseEntity<Object> cancelarBoleto(@PathVariable String id) {
+
+		Optional<Boleto> boletoOptional = service.buscarPorId(id);
+
+		if (!boletoOptional.isPresent())
+			return ResponseEntity.notFound().build();
+		
+		boletoOptional.get().setStatus(EnumStatus.CANCELED);
+		service.salvar(boletoOptional.get());
+
+		return ResponseEntity.noContent().build();
+	}
 	
 }
