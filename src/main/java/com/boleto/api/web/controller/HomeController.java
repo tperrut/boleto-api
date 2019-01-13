e33cfece9c86772ee70712d45427b0934b581343
list <41409309+tperrut@users.noreply.github.com>
2019-01-13 10:05:14 -0200

    Refactor nas Anotações

--- src/main/java/com/boleto/api/web/controller/HomeController.java
+++ src/main/java/com/boleto/api/web/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.boleto.api.web.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 	
-	@RequestMapping(path="/",method=RequestMethod.GET ,produces= {"application/JSON"})
+	@GetMapping(path="/",produces=MediaType.APPLICATION_JSON_VALUE)
 	public String home() {
 		return "{\"success\":\"Seja Bem vindo ao Boleto-API\"}";
 	}
