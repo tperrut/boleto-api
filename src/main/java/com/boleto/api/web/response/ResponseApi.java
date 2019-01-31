ef645a04a8ff98432e4b7344357b6b9935d9b461
list <41409309+tperrut@users.noreply.github.com>
2019-01-30 22:58:11 -0200

    Correção texto msg validação

---
+++ src/main/java/com/boleto/api/web/response/ResponseApi.java
@@ -0,0 +1,22 @@
+package com.boleto.api.web.response;
+
+import java.util.List;
+
+public class ResponseApi<T> {
+	private T data;
+	private List<String> erros;
+	
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+	public List<String> getErros() {
+		return erros;
+	}
+	public void setErros(List<String> erros) {
+		this.erros = erros;
+	}
+	
+}
