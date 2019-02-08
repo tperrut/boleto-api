c289432c49d3e8857a99ecd243ed4276df4b48c5
list <41409309+tperrut@users.noreply.github.com>
2019-02-08 00:11:46 -0200

    Add lombok + mudança de List pra Set + inicializaão da coleçtion de erros

--- src/main/java/com/boleto/api/web/response/ResponseApi.java
+++ src/main/java/com/boleto/api/web/response/ResponseApi.java
@@ -1,23 +1,16 @@
 package com.boleto.api.web.response;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResponseApi<T> {
 	
 	private T data;
-	private List<String> erros;
-	
-	public T getData() {
-		return data;
-	}
-	public void setData(T data) {
-		this.data = data;
-	}
-	public List<String> getErros() {
-		return erros;
-	}
-	public void setErros(List<String> erros) {
-		this.erros = erros;
-	}
+	private Set<String> erros = new HashSet<String>();
 	
 }
