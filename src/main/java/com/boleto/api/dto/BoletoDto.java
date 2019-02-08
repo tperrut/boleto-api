c2005ae28f98b12d36c827bc2e3ae7a749ee0318
list <41409309+tperrut@users.noreply.github.com>
2019-02-08 00:02:06 -0200

    Criação do Dto para padronizar a criação de boleto.

---
+++ src/main/java/com/boleto/api/dto/BoletoDto.java
@@ -0,0 +1,36 @@
+package com.boleto.api.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import com.boleto.api.model.Boleto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter @Setter 
+public class BoletoDto {
+	
+	@NotNull(message = "Total não pode ser vazio")
+	private BigDecimal total;
+	
+	@NotEmpty(message = "Cliente não pode ser vazio")
+	private String cliente;
+	
+	@NotNull(message="Data Vencimento não pode ser vazio")
+	private LocalDate dataVencimento;
+	
+	public BoletoDto(String cli, LocalDate dtVencimento, BigDecimal totalParam) {
+		this.total=totalParam;
+		this.dataVencimento= dtVencimento;
+		this.cliente= cli;
+	}
+
+	public Boleto converteBoletoDtoToBoleto(BoletoDto dto) {
+		return new Boleto(dto.getCliente(),dto.getDataVencimento(),dto.getTotal());
+	}
+}
