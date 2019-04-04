c6ebcca0765f3f10f8458961f67e3d5ffa46f7f8
list <41409309+tperrut@users.noreply.github.com>
2019-04-04 11:52:47 -0300

    add form login default spring security

---
+++ src/main/java/com/boleto/api/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.boleto.api.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+		.and()
+		.formLogin();
+	}
+}
